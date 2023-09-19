ff9d2cdb60d59c11c73b3af248b88249ea580452
Austin Huang <austin.huang@duke.edu>
2023-09-19 16:55:24 -0400

    worked on Home and About pages

---
+++ index.js
@@ -0,0 +1,14 @@
+var flower_head = document.getElementById("head");
+window.addEventListener("scroll", function() {
+    flower_head.style.transform = "rotate("+window.pageYOffset+"deg)";
+});
+
+var flower_head1 = document.getElementById("head1");
+window.addEventListener("scroll", function() {
+    flower_head1.style.transform = "rotate("+window.pageYOffset+"deg)";
+});
+
+var flower_head2 = document.getElementById("head2");
+window.addEventListener("scroll", function() {
+    flower_head2.style.transform = "rotate("+window.pageYOffset+"deg)";
+});
